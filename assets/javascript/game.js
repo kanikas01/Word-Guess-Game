dee1f92ae52c0d9bce804d750cd9e8fcaa309bf8
kanikas01 <larvakurt@yahoo.com>
2018-12-18 21:55:48 -0800

    Put more code in functions, continue to refine code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-// Assign variables to each important paraagraph tag
+// Assign variables to each important paragraph tag
 var winsPara = document.getElementById("wins");
 var currentWordPara = document.getElementById("current-word");
 var guessesRemainingPara = document.getElementById("guesses-remaining");
@@ -13,49 +13,75 @@ var wins = 0;
 
 // Array to hold our cat breeds
 var catBreeds = ["Abyssinian", "American Bobtail", "American Curl", "American Shorthair", "American Wirehair", "Balinese", "Bengal Cats", "Birman", "Bombay", "British Shorthair", "Burmese", "Burmilla", "Chartreux", "Chinese Li Hua", "Colorpoint Shorthair", "Cornish Rex", "Cymric", "Devon Rex", "Egyptian Mau", "European Burmese", "Exotic", "Havana Brown", "Himalayan", "Japanese Bobtail", "Javanese", "Korat", "LaPerm", "Maine Coon", "Manx", "Nebelung", "Norwegian Forest", "Ocicat", "Oriental", "Persian", "Pixie-Bob", "Ragamuffin", "Ragdoll Cats", "Russian Blue", "Savannah", "Scottish Fold", "Selkirk Rex", "Siamese Cat", "Siberian", "Singapura", "Snowshoe", "Somali", "Sphynx", "Tonkinese", "Turkish Angora", "Turkish Van"];
-// Choose random cat breed
-breed = catBreeds[Math.floor(Math.random() * catBreeds.length)];
-console.log(breed); // for testing
 
-// Display initial values for game start
-currentWordPara.innerHTML = showBlankWord(breed);
-guessesRemainingPara.innerHTML = guessesRemaining;
+//---------- FUNCTION DEFINITIONS ----------//
 
-document.onkeyup = function (event) {
-  var guess = event.key.toUpperCase();
-  lettersGuessedPara.innerHTML += guess + ' ';
-  guessesRemainingPara.innerHTML = --guessesRemaining;
-
-  if (isSolved == true) {
-    //Increment and display 'wins'
-    winsPara.innerHTML = ++wins;
-    reset();
-  }
-  else if ((isSolved == false) && (guessesRemaining == -1)) {
-    reset();
-  }
+// Select random array element (for getting random cat breed)
+function getRandomArrayElement(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-function reset() {
-  // Reset number of guesses
+// Initialize variables for next round
+function initialize() {
+  // Reset all values
   guessesRemaining = maxGuesses;
-  // Erase last game's info from the DOM
+  isSolved = false;
+
+  // Reset DOM display
   guessesRemainingPara.innerHTML = guessesRemaining;
   lettersGuessedPara.innerHTML = '';
+
   // Choose a new cat
-  breed = catBreeds[Math.floor(Math.random() * catBreeds.length)];
+  breed = getRandomArrayElement(catBreeds);
+  console.log(breed); // for testing
+  
+  // Show unknown word as series of underscores
+  currentWordPara.innerHTML = showBlankWord(breed);
 }
 
-function showBlankWord (word) {
+// Show hidden word as underscores
+function showBlankWord(word) {
   var hiddenWord = '';
   for (var i = 0; i < word.length; i++) {
-    // If breed contains a blank, omit underscore
+    // Omit underscore for blank spaces
     if (word[i] == ' ') {
       hiddenWord += '&nbsp;&nbsp';
-    } 
+    }
     else {
       hiddenWord += "_ ";
     }
   }
   return hiddenWord.trim();
+}
+
+function testGuess(guess) {
+  // WRITE ME
+  return;
+}
+
+function checkProgress() {
+  // WRITE ME
+  return false;
+}
+
+//---------- END FUNCTION DEFINITIONS ----------//
+
+initialize();
+
+document.onkeyup = function (event) {
+  var guess = event.key.toUpperCase();
+  lettersGuessedPara.innerHTML += guess + ' ';
+  guessesRemainingPara.innerHTML = --guessesRemaining;
+  var result = testGuess();
+  isSolved = checkProgress();
+  console.log(result);
+
+  if (isSolved == true) {
+    //Increment and display 'wins'
+    winsPara.innerHTML = ++wins;
+    initialize();
+  }
+  else if ((isSolved == false) && (guessesRemaining == -1)) {
+    initialize();
+  }
 }
