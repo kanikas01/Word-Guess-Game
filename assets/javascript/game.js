d26e359beb2f609ada2c0748d1e43718bbad4ac8
kanikas01 <larvakurt@yahoo.com>
2018-12-21 18:55:54 -0800

    Update comments, spacing and variable names

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,69 +12,70 @@ var h1 = document.getElementById("h1");
 var gamePrompt = document.getElementById("game-prompt");
 
 // Declare and initialize additional variables
-const maxGuesses = 7;
-var isSolved = false;
-var guessesRemaining = maxGuesses;
-var wins = 0;
-var currentWord = '';
-var hiddenWord = '';
-var happyMeow = new Audio('assets/sounds/meow.mp3');
-var angryMeow = new Audio('assets/sounds/angry-meow.mp3');
-var letterChoices = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lettersGuessed = [' ']; // Space character is given in case of multi-word answers
+const maxWrongGuesses = 7;  // Max number of wrong guesses allowed before losing game
+var isSolved = false;       // Has the puzzle been solved
+var guessesRemaining = 0;   // Number of wrong guesses remaining before losing game
+var wins = 0;               // Number of wins in session
+var currentWord = '';       // The word being guessed
+var hiddenWord = '';        // currentWord with unguessed letters replaced by underscores
+var lettersGuessed = [];    // Holds letters guessed during gameplay
+var letterChoices = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';   // All possible legit key inputs
+var happyMeow = new Audio('assets/sounds/meow.mp3');        // Happy cat sound
+var angryMeow = new Audio('assets/sounds/angry-meow.mp3');  // Angry cat sound
 
 // Create array to hold our cat breeds
 var catBreeds = ["Abyssinian",
-  "American Bobtail",
-  "American Curl",
-  "American Shorthair",
-  "American Wirehair",
-  "Balinese",
-  "Bengal",
-  "Birman",
-  "Bombay",
-  "British Shorthair",
-  "Burmese",
-  "Burmilla",
-  "Chartreux",
-  "Chinese Li Hua",
-  "Colorpoint Shorthair",
-  "Cornish Rex",
-  "Cymric",
-  "Devon Rex",
-  "Egyptian Mau",
-  "European Burmese",
-  "Havana Brown",
-  "Himalayan",
-  "Japanese Bobtail",
-  "Javanese",
-  "Korat",
-  "LaPerm",
-  "Maine Coon",
-  "Manx",
-  "Nebelung",
-  "Norwegian Forest",
-  "Ocicat",
-  "Persian",
-  "Ragamuffin",
-  "Ragdoll",
-  "Russian Blue",
-  "Savannah",
-  "Scottish Fold",
-  "Selkirk Rex",
-  "Siamese",
-  "Siberian",
-  "Singapura",
-  "Snowshoe",
-  "Somali",
-  "Sphynx",
-  "Tonkinese",
-  "Turkish Angora",
-  "Turkish Van"];
+                "American Bobtail",
+                "American Curl",
+                "American Shorthair",
+                "American Wirehair",
+                "Balinese",
+                "Bengal",
+                "Birman",
+                "Bombay",
+                "British Shorthair",
+                "Burmese",
+                "Burmilla",
+                "Chartreux",
+                "Chinese Li Hua",
+                "Colorpoint Shorthair",
+                "Cornish Rex",
+                "Cymric",
+                "Devon Rex",
+                "Egyptian Mau",
+                "European Burmese",
+                "Havana Brown",
+                "Himalayan",
+                "Japanese Bobtail",
+                "Javanese",
+                "Korat",
+                "LaPerm",
+                "Maine Coon",
+                "Manx",
+                "Nebelung",
+                "Norwegian Forest",
+                "Ocicat",
+                "Persian",
+                "Ragamuffin",
+                "Ragdoll",
+                "Russian Blue",
+                "Savannah",
+                "Scottish Fold",
+                "Selkirk Rex",
+                "Siamese",
+                "Siberian",
+                "Singapura",
+                "Snowshoe",
+                "Somali",
+                "Sphynx",
+                "Tonkinese",
+                "Turkish Angora",
+                "Turkish Van"];
 
 //---------- FUNCTION DEFINITIONS ----------//
 
 // Select random array element (for getting random cat breed)
+// Takes an array argument
 function getRandomArrayElement(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)].toUpperCase();
 }
@@ -86,7 +87,7 @@ function initialize() {
   result.style.display = 'none';
   h1.innerHTML = "Guess the cat breed";
   gamePrompt.innerHTML = "Type any letter to get started!";
-  guessesRemaining = maxGuesses;
+  guessesRemaining = maxWrongGuesses;
   isSolved = false;
   lettersGuessed = [];
 
@@ -104,6 +105,7 @@ function initialize() {
 }
 
 // Show hidden word as underscores (except space and hyphen chars)
+// 'word' - the word currently being guessed
 function getBlankWord(word) {
   var blankWord = '';
   for (var i = 0; i < word.length; i++) {
@@ -121,6 +123,7 @@ function getBlankWord(word) {
 }
 
 // Create version of the blank word that is more legible
+// 'word' - the word currently being guessed
 function displayBlankWord(word) {
   var blankWord = '';
   for (var i = 0; i < word.length; i++) {
@@ -135,6 +138,9 @@ function displayBlankWord(word) {
 }
 
 // Update blank word with each correct guess
+// 'word' - the word currently being guessed
+// 'blankWord' - the hidden form of word, likely 'hiddenWord'
+// 'letter' - the current letter guess
 function updateBlankWord(word, blankWord, letter) {
   ;
   var newBlankWord = '';
@@ -150,10 +156,13 @@ function updateBlankWord(word, blankWord, letter) {
 }
 
 // Check whether the word has been guessed
+// 'word' - the word currently being guessed
 function checkProgress(word) {
   return !(word.includes('_'));
 }
-
+// Displays results depending on win or loss
+// 'outcome' - true for win, false for lose
+// 'word' - the word currently being guessed
 function displayResults (outcome, word) {
   if (outcome) {
     happyMeow.play();
